ebb66980380a9c80c00abd28d370a6839937180c
ilyakoni <ilyakoni@gmail.com>
2022-09-25 10:33:32 +0600

    many changes!

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 let app = express();
 const fs = require('fs')
@@ -6,6 +7,7 @@ const open = require('open')
 const { spawnSync } = require ('child_process');
 const url = require('url')
 const http=require('http')
+
 //Создаем Static для определения корневой папки сайта
 app.use('/',express.static(__dirname + '/public'));
 
@@ -20,7 +22,8 @@ app.post('/upload',upload.any('photos'), function(req, res) {
     res.redirect('/results')
 });
 app.get('/results', function(req, res){
-    res.write('<link rel="stylesheet" href="style.css"><script src="script.js"></script>')
+    res.write(`<meta charset="utf-8"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><link rel="stylesheet" href="style.css"> <script src="script.js"></script><link rel="stylesheet" href="/results/bootstrap-magnify/css/bootstrap-magnify.css"> `)
     console.log('hello')
     fs.readdir(__dirname+'/public/results/upload', function(err, files) {
         if (err) throw err;
@@ -34,13 +37,15 @@ app.get('/results', function(req, res){
     });
 
     function showImages(images) {
+        res.write('<table class="table table-image"><tr><th scope="col">id</th><th scope="col">Изображение</th><th scope="col">Путь</th></tr>')
         for (i = 0; i < images.length; i++) {
-            res.write(`<div class='imageContainer'><a target="_blank" href='/results/upload/${images[i]}' type="image"><img width=512px height=512px src='/results/upload/${images[i]}' align=bottom></a><p>${__dirname+'results/upload/'+images[i]}</p></div>`);
+            
+            res.write(`<th scope="row">${i}</th><td><div class='imageContainer'><a target="_blank" href='/results/upload/${images[i]}' type="image"><img data-toggle="magnify" class="img-fluid img-thumbnail" src="/results/upload/${images[i]}"/></a></td><td><p>${__dirname+'/results/upload/'+images[i]}</p></div></td></tr>`);
+
         }
         
-        for (i=0; i<images.length;i++){
-            res.write(`<script type="text/javascript">magnify(5,${i})</script>`)
-    }
+        res.write(`<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script><script src="/results/bootstrap-magnify/js/bootstrap-magnify.min.js"></script>  `)
+        
         res.end();
     }
 }
